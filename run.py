63601e4d9a74bde4136f85ac334cd25ac8fd820e
Scott Atkinson <scott.atkin79@gmail.com>
2022-02-12 12:11:00 +0000

    create game function

--- run.py
+++ run.py
@@ -71,6 +71,22 @@ def print_6_mistakes():
     print("/ | \\      ")
     
 
+def print_game_status():
+    if mistakes == 0:
+        print_0_mistakes()
+        elif mistake == 1:
+            print_1_mistakes()
+        elif mistake == 2:
+            print_2_mistakes()
+        elif mistake == 3:
+            print_3_mistakes()
+        elif mistake == 4:
+            print_4_mistakes()
+        elif mistake == 5:
+            print_5_mistakes()
+        elif mistake == 6:
+            print_6_mistakes()
+
 # Create list of words 
 words = ["hangman", "band", "house", "stereo", "tree", "swim", "creator"]
 # Create letters tried
