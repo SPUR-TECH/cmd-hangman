d8b9dec323a6d98e90021e76020ed6450231a4da
Scott Atkinson <scott.atkin79@gmail.com>
2022-02-12 10:53:59 +0000

    create a list of words, import random, and create lives and mistakes

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# create list of words 
+words = ["HANGMAN", "WINDOW", "HOUSE", "ADVENTURE", "MOBILE", "PLAYGROUND", "MOUNTAIN", "COUNTRY", "BUILDING", "STEREO", "PASSION", "SHOPPING", "TRADER", "SWIMMING", "PLAYING"]
+# create letters tried
+tries = []
+# lives
+ties_left = 6
+#mistakes
+mistakes = 0
+
+
+
