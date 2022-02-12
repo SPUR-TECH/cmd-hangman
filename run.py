55e614b130de387583ce533e786a9e4d417cf619
Scott Atkinson <scott.atkin79@gmail.com>
2022-02-12 11:23:34 +0000

    create random word

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 import random
 
-# create list of words 
-words = ["HANGMAN", "WINDOW", "HOUSE", "ADVENTURE", "MOBILE", "PLAYGROUND", "MOUNTAIN", "COUNTRY", "BUILDING", "STEREO", "PASSION", "SHOPPING", "TRADER", "SWIMMING", "PLAYING"]
-# create letters tried
+# Create list of words 
+words = ["hangman", "band", "house", "stereo", "tree", "swim",]
+# Create letters tried
 tries = []
-# lives
-ties_left = 6
-#mistakes
+# Lives tally
+tries_left = 6
+# Mistakes tally
 mistakes = 0
-
+# Randomize word selection
+word_index = random.randint(0, len(words)-1)
+word = words[word_index].upper()
+print(word)
 
 
