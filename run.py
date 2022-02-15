e23807791f40b9aec7d6ad4feda8c889b312c5cf
Scott Atkinson <scott.atkin79@gmail.com>
2022-02-15 20:10:08 +0000

    fix restart issue

--- run.py
+++ run.py
@@ -91,7 +91,7 @@ def print_6_mistakes():
 
 
 # function to print game status
-def print_game_status():
+def print_game_status(mistakes, guesses, remaining_guesses):
     if mistakes == 0:
         print_0_mistakes()
     elif mistakes == 1:
@@ -115,79 +115,87 @@ def print_game_status():
     print(f"\nYou have {remaining_guesses} guess(es) left")
 
 
-# Create list of words
-words = ["hangman", "band", "house", "stereo", "tree", "swim", "creator"]
-
-# Create alphabet
-alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-# Create letters tried
-guesses = []
-
-# Create used letters
-used_letters = []
-
-# Guesses left
-remaining_guesses = 6
-
-# Mistakes made
-mistakes = 0
-
-# Randomize word selection
-word_index = random.randint(0, len(words)-1)
-word = words[word_index].upper()
-print(word)
-
-# Append an underscore for user to see as blank letter
-for i in range(len(word)):
-    guesses.append('_')
-
 # Create game loop
-game_over = False
+def play_game():
 
-while not game_over:
-    print_game_status()
+    # Create list of words
+    words = ["hangman", "band", "house", "stereo", "tree", "swim", "creator"]
 
-# Users guesses
-    user_input = input("Please enter a letter:\n")
-    letter = user_input.upper()
+    # Create alphabet
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-# Display error message if not a letter
-    if letter not in alphabet:
-        print("Thats not a letter. Please try again")
-    elif letter in used_letters:
-        print('"ALREADY USED!!"')
-    else:
-        used_letters += user_input.upper()
-        print(f'"USED LETTERS:" {used_letters}')
-        # Display win message if all letters guessed
-        if letter in word:
-            for i in range(len(word)):
-                if word[i] == letter:
-                    guesses[i] = letter
-            if '_' not in guesses:
-                print(f"YOU WIN!!! the word was '{word}'")
-                game_over = True
-        # Display try again message if letter not in word and
-        #  lose message when all tries are used and end game
+    # Create letters tried
+    guesses = []
+
+    # Create used letters
+    used_letters = []
+
+    # Guesses left
+    remaining_guesses = 6
+
+    # Mistakes made
+    mistakes = 0
+
+        # Randomize word selection
+    word_index = random.randint(0, len(words)-1)
+    word = words[word_index].upper()
+    print(word)
+
+    # Append an underscore for user to see as blank letter
+    for i in range(len(word)):
+        guesses.append('_')
+
+
+    game_over = False
+    
+    while not game_over:
+        print_game_status(mistakes, guesses, remaining_guesses)
+
+    # Users guesses
+        user_input = input("Please enter a letter:\n")
+        letter = user_input.upper()
+
+    # Display error message if not a letter
+        if letter not in alphabet:
+            print("Thats not a letter. Please try again")
+        elif letter in used_letters:
+            print('"ALREADY USED!!"')
         else:
-            print("sorry, try again")
-            remaining_guesses -= 1
-            mistakes += 1
-            if mistakes == 6:
-                print_6_mistakes()
-                print(f"You lose! the word was '{word}'")
-                game_over = True
+            used_letters += user_input.upper()
+            print(f'"USED LETTERS:" {used_letters}')
+            # Display win message if all letters guessed
+            if letter in word:
+                for i in range(len(word)):
+                    if word[i] == letter:
+                        guesses[i] = letter
+                if '_' not in guesses:
+                    print(f"YOU WIN!!! the word was '{word}'")
+                    game_over = True
+            # Display try again message if letter not in word and
+            #  lose message when all tries are used and end game
+            else:
+                print("sorry, try again")
+                remaining_guesses -= 1
+                mistakes += 1
+                if mistakes == 6:
+                    print_6_mistakes()
+                    print(f"You lose! the word was '{word}'")
+                    game_over = True
+            
+    menu()
 
-if __name__ == '__main__':
-    while True:
-        print("Do you want to play again?")
-        print('Y/N')
-        answer = input()
-        print(answer)
-        if answer.upper == 'Y':
-            game_over = False
-        if answer.upper == 'N':
-            print("come back soon!!")
-        break
+def menu():
+    print("Do you want to play again?")
+    print('Y/N')
+    answer = input()
+    print(answer)
+    if answer.upper() == 'Y':
+        play_game()
+    if answer.upper() == 'N':
+        print("come back soon!!")
+
+
+play_game()
+
+       
